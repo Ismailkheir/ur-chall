27292e8a332dd1eb3aa936cfb4c786db725321a4
Ismailkheir <ismailkheirbn@gmail.com>
2019-12-13 11:41:05 +0100

    Using React Hooks + Scrolling to the bottom of the list function

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Repo from "./Repo";
 import axios from "axios";
 
-class RepoList extends React.Component {
-  state = {
-    repos: []
-  };
+const RepoList = () => {
+  const [repos,setRepos] = useState([])
+  
+  function handleScroll() {
+    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+    console.log("End of list, fetch more");
+  }
 
-  //HTTP Request using axios
-  componentDidMount() {
+  useEffect(() => {
+    //Fetching data
     const url =
-      "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc";
-    axios.get(url).then(res => this.setState({repos: res.data.items}));
-  }
+    "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc";
+    axios.get(url).then(res => setRepos(res.data.items));
+    //Scroll event listner
+    window.addEventListener('scroll',handleScroll);
+    return () => window.removeEventListener('scroll',handleScroll);
+  }, [])
 
-  render() {
-    console.log(this.state.repos);
-    //Render chaque element de l'array 'repos' (repo={item} : props)
-    return this.state.repos.map(item => <Repo repo={item} key={item.id} />);
-  }
+  //Render chaque element de l'array 'repos' (repo={item} : props)
+  return repos.map(item => <Repo repo={item} key={item.id} />);
 }
 
 export default RepoList;
