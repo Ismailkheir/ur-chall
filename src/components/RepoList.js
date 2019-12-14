a61b56cc9a7b747ed5f42de8cfb9bf4d1eb158cb
Ismailkheir <ismailkheirbn@gmail.com>
2019-12-14 17:56:45 +0100

    Infinite Scrolling feature

--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -3,25 +3,37 @@ import Repo from "./Repo";
 import axios from "axios";
 
 const RepoList = () => {
-  const [repos,setRepos] = useState([])
-  
+  const [repos, setRepos] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [page, setPage] = useState(1);
+
+  function fetchMoreRepos() {
+    const url = `https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${page}`;
+    axios.get(url).then(res => setRepos([...repos, ...res.data.items]));
+    setPage(page + 1);
+    setIsFetching(false);
+  } 
+  //Whenever we get to the bottom of the page we set 'isFetching' to true to fetch more data
   function handleScroll() {
     if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
-    console.log("End of list, fetch more");
+    setIsFetching(true);
   }
+  //if isFetching is true (meaning we are at the bottom of the page) we fetch more data
+  useEffect(() => {
+    if(!isFetching) return;
+    fetchMoreRepos();
+    }, [isFetching]);
 
   useEffect(() => {
-    //Fetching data
-    const url =
-    "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc";
-    axios.get(url).then(res => setRepos(res.data.items));
-    //Scroll event listner
-    window.addEventListener('scroll',handleScroll);
-    return () => window.removeEventListener('scroll',handleScroll);
-  }, [])
+  //Fetch data when the component mounts for the first time
+  fetchMoreRepos();
+  //Scroll event listner
+  window.addEventListener('scroll',handleScroll);
+  return () => window.removeEventListener('scroll',handleScroll);
+  }, []);
 
-  //Render chaque element de l'array 'repos' (repo={item} : props)
-  return repos.map(item => <Repo repo={item} key={item.id} />);
+  //Render chaque element de l'array 'repos'
+  return repos.map((item,index) => <Repo repo={item} key={index} />);
 }
 
 export default RepoList;
